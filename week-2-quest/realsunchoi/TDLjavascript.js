dcb8448be849586ac6fc367f329a51d18ce8e151
Jinseon Choi <oooihmm@naver.com>
2022-07-15 21:12:15 +0900

    add check/uncheck/delate All

--- week-2-quest/realsunchoi/TDLjavascript.js
+++ week-2-quest/realsunchoi/TDLjavascript.js
@@ -1,5 +1,4 @@
-
-function getInputByEnter(){ //enter 버튼 클릭시 할 일 추가
+function getInputByButton(){ //enter 버튼 클릭시 할 일 추가
   let addPlan = document.createElement('li');
   let plan =  document.getElementById('input').value;
   addPlan.innerText = plan;
@@ -21,4 +20,33 @@ function getInputByEnter(){ //enter 버튼 클릭시 할 일 추가
     modifyButton.appendChild(addModifyButton);
     delateButton.appendChild(addDelateButton);
   }
+}
+
+function getInputByEnter(e){ // enter 키 입력시 할 일 추가
+  if(e.key == "Enter"){
+    getInputByButton();
+  }
+}
+
+function checkAll(){
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {checkbox.checked = true;})
+}
+
+function uncheckAll(){
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {checkbox.checked = false;})
+}
+
+function delateAll(){
+  while(checkbox.hasChildNodes()){
+    checkbox.removeChild(checkbox.firstChild);
+    toDoList.removeChild(toDoList.firstChild);
+    modifyButton.removeChild(modifyButton.firstChild);
+    delateButton.removeChild(delateButton.firstChild);
+  }
+}
+
+function checkall(){
+
 }
