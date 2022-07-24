ab9fd739a1821ea6adb7fcda88a7ecf1e49ab4da
jun <990114js@naver.com>
2022-07-25 02:31:39 +0900

    onclick MyFile, recent

---
+++ week-3-quest/onedrive-shin/onedrive.js
@@ -0,0 +1,10 @@
+// 왼쪽 버튼 클릭시 해당 html로 이동
+function myfile_click() {
+    window.location.href = 'onedrive_MyFile.html'
+}
+
+function recent_click()  {
+    window.location.href = 'onedrive_recent.html'
+}
+
+
