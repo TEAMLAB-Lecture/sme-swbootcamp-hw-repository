ae62a96cee1bd7318a550cda62b8ca2334dff84b
jun <990114js@naver.com>
2022-07-25 03:06:42 +0900

    function nav button half success

--- week-3-quest/onedrive-shin/onedrive.js
+++ week-3-quest/onedrive-shin/onedrive.js
@@ -2,11 +2,15 @@
 function myfile_click() {
     window.location.href = 'onedrive_MyFile.html'
 }
-
 function recent_click()  {
     window.location.href = 'onedrive_recent.html'
 }
 function trash_click()  {
     window.location.href = 'onedrive_trash.html'
 }
-
+function share_forMe_click()  {
+    window.location.href = 'onedrive_share_forMe.html'
+}
+function share_me_click()  {
+    window.location.href = 'onedrive_share.html'
+}
